8f4fed1dcbc654380fdb9fb584627787c4e78f6b
bugkwuynx <hngo_2028@depauw.edu>
2025-03-12 09:28:32 -0400

    finalize backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,20 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        
+        mongoose.connection.on('error', err => {
+            console.error('MongoDB connection error:', err);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected. Attempting to reconnect...');
+        });
+
     } catch (error) {
-        console.log("Error connecting to MongoDB", error);
+        console.error("MongoDB connection error:", error.message);
         process.exit(1);
     }
 }
